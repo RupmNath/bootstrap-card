e9b8f399330958d086dd16736b2653d25e405eb7
RupmNath <128634103+RupmNath@users.noreply.github.com>
2025-02-23 21:26:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import BootStrapCard from './components/BootStrapCard';
+
+function App() {
+
+  return (
+    <>
+      <div className='container mt-4 p-3'>
+      <BootStrapCard/>
+      </div>
+    </>
+  )
+}
+
+export default App
